3fd30df4b05e7b7e0d0a1493224944c9d43c7090
bamr87 <bamr87@users.noreply.github.com>
2024-09-17 12:10:11 -0600

    Powered by template

--- parodynews/templatetags/custom_filters.py
+++ parodynews/templatetags/custom_filters.py
@@ -6,3 +6,10 @@ register = template.Library()
 @register.filter
 def get_field_value(instance, field_name):
     return getattr(instance, field_name, None)
+
+# truncate_chars filter
+@register.filter
+def truncate_chars(value, max_length):
+    if isinstance(value, str) and len(value) > max_length:
+        return value[:max_length] + '...'
+    return value
